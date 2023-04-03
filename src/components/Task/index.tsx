211dd4f4f0015e0e95272bb8174da3910385eb76
dgeoi <diegofreitas_oliveira@proton.me>
2023-04-03 18:34:10 -0300

    Update [src/components/Task]: Checkmarks no longer a input and now has the check icon when completed

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,5 +1,5 @@
-import { Trash } from 'phosphor-react';
-import { ChangeEvent, useState } from 'react';
+import { Check, Trash } from 'phosphor-react';
+import { MouseEvent, useState } from 'react';
 import styles from './Task.module.css';
 
 export interface TaskShape {
@@ -14,20 +14,24 @@ type TaskProps = {
 export function Task({task}: TaskProps) {
   const [isTaskCompleted, setIsTaskCompleted] = useState<boolean>(task.isComplete);
 
-  function handleTaskCheckbox(event: ChangeEvent<HTMLInputElement>) {
-    setIsTaskCompleted(event.currentTarget.checked);
+  function handleTaskCompletion(event: MouseEvent<HTMLElement>) {
+    event.preventDefault();
+    setIsTaskCompleted(previous => !previous);
   }
 
   return (
     <article className={styles.task}>
-      <input
-        type="checkbox"
-        checked={isTaskCompleted}
-        className={styles.taskCheckbox}
-        onChange={(event) => {handleTaskCheckbox(event);}}
-      />
+      <div className={styles.taskCheckmarkArea} onClick={(event) => {handleTaskCompletion(event);}}>
+        {isTaskCompleted ? 
+          <span className={styles.taskCheckmarkForCompletedOnes}>
+            <Check weight='bold' size={12} />
+          </span>
+          :
+          <div className={styles.taskCheckmarkForNotCompletedOnes} />
+        }
+      </div>
 
-      <p className={isTaskCompleted ? styles.taskDescriptionCompleted : styles.taskDescription}>{task.description}</p>
+      <p className={isTaskCompleted ? styles.taskDescriptionForCompletedOnes : styles.taskDescriptionForNotCompletedOnes}>{task.description}</p>
 
       <button className={styles.taskDeleteButton}>
         <span><Trash size={18} /></span>
